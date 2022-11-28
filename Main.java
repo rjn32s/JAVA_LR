5dd51563263378f6a332956401566558ae8b9ccc
Rajan Shukla <94681756+rjn32s@users.noreply.github.com>
2022-11-28 02:54:02 +0000

    Array read and print

--- Main.java
+++ Main.java
@@ -1,5 +1,24 @@
+
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!!!!");
+    
+    Scanner input = new Scanner(System.in);
+    
+    int[][] arr = new int[3][3];
+
+    for (int row =0;row < arr.length;row++){
+      for (int col =0;col < arr[row].length;col++){
+        arr[row][col] = input.nextInt();
+      }
+    }
+
+    for (int[] j : arr){
+      for (int k : j){
+      System.out.print(k+" ");
+    }
+      System.out.println(" ");
+  }
   }
 }
